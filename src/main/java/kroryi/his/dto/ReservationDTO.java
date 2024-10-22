4a6cebd6e3122ab2a59d67c9ddfbc52c78d60713
kwanho <ds60303@naver.com>
2024-10-22 22:00:49 +0900

    의사,치료유형 db추가

--- src/main/java/kroryi/his/dto/ReservationDTO.java
+++ src/main/java/kroryi/his/dto/ReservationDTO.java
@@ -3,8 +3,6 @@ package kroryi.his.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 
 @Setter
 @Getter
@@ -21,12 +19,12 @@ public class ReservationDTO {
         super();
     }
 
-    public ReservationDTO(Long seq, LocalDateTime reservationDate,
+    public ReservationDTO(Long seq, String reservationDate,
                           String department, boolean snsNotification, String chartNumber, String patientNote,
                           String reservationStatusCheck) {
         super();
         this.seq = seq;
-        this.reservationDate = String.valueOf(reservationDate);
+        this.reservationDate = reservationDate;
         this.department = department;
         this.snsNotification = snsNotification;
         this.chartNumber = chartNumber;
